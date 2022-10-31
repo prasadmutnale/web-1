ac38b03581e10dfddf85ec0ef4d43e75146d115c
prasadmutnale <66254117+prasadmutnale@users.noreply.github.com>
2022-11-01 00:57:35 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-gcjg7h)
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import About from './components/About';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
